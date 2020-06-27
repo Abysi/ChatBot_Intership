b0c25f088367dda685100d5397de6a922dc1d2f2
Andrey Rudenko <andreyrudenko2101@gmail.com>
2020-06-27 12:30:40 +0300

    Added logger

---
+++ src/helpers/logger/winston.js
@@ -0,0 +1,13 @@
+const winston = require("winston");
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "user-service" },
+  transports: [
+    // - Write all logs below to `winstonLog.log`
+    new winston.transports.File({ filename: "winstonLog.log" }),
+  ],
+});
+
+module.exports = logger;
